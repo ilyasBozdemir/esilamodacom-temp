c75ff00b5c16638b989ec3e76a8a74a197bf85c6
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-28 20:14:57 +0300

    updated

---
+++ dist/_next/static/chunks/pages/linktree-4719ad8bc8cbcff9.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[92],{2718:function(n,u,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/linktree",function(){return i(2062)}])},2062:function(n,u,i){"use strict";i.r(u),i.d(u,{__N_SSP:function(){return e}});var r=i(5893);i(1163),i(7294);var e=!0;u.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Y\xf6nlendiriliyorsunuz..."})})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2718)}),_N_E=n.O()}]);
+//# sourceMappingURL=linktree-4719ad8bc8cbcff9.js.map
--- pages/linktree.jsx
+++ pages/linktree.jsx
@@ -2,9 +2,22 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 function Linktree() {
-  useEffect(() => {
-    window.location.href = "https://heylink.me/esilamoda/";
-  }, []);
+  return (
+    <div>
+      <h1>Yönlendiriliyorsunuz...</h1>
+      {/* Opsiyonel olarak yönlendirme olmadan önce burada içerik gösterebilirsiniz */}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "https://heylink.me/esilamoda/",
+      permanent: false,
+      replace: true,
+    },
+  };
 }
 
 export default Linktree;
